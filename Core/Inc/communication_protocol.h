ef7a5cc2746e8b71f1e9e06afa9f08a171985a4d
Błażej Drozd <drozdbazej@gmail.com>
2024-11-08 19:35:54 +0100

    Full frame decoding

--- Core/Inc/communication_protocol.h
+++ Core/Inc/communication_protocol.h
@@ -17,7 +17,13 @@
 #define CP_END_CHAR 		0x7D
 #define CP_ENCODE_CHAR 		0x5C
 #define CP_MAX_DATA_LEN		200
-#define CP_MIN_DATA_LEN		12
+#define CP_MIN_FRAME_LEN	12
+#define CP_DATA_START_IDX	7
+#define CP_STM_REC_ID		0xFF
+
+#define CP_START_CODE_CHAR	0x30
+#define CP_END_CODE_CHAR	0x31
+#define CP_ENCODE_CODE_CHAR	0x32
 
 typedef enum {
 	WAIT_FOR_START,
@@ -29,21 +35,41 @@ typedef enum {
 	WAIT_FOR_END
 } FrameState;
 
+typedef enum {
+	DS_READ_DATA,
+	DS_DECODE_NEXT_DATA
+} CP_DecodeState_t;
+
+typedef enum {
+	DR_OK,
+	DR_DECLARED_DATA_LEN_NOT_EQUAL_TO_PROVIDED,
+	DR_WRONG_DATA_LEN_PROVIDED_IN_FRAME,
+	DR_WRONG_NEXT_CHARACTER,
+	DR_DATA_END_WHILE_DECODE,
+	DR_FRAME_TOO_SHORT,
+	DR_RECEIVER_EQUAL_SENDER,
+	DR_RECEIVER_DIFF,
+	HEX_WRONG_CHAR,
+	HEX_OK,
+	V_OK,
+	V_CRC_ERROR
+} CP_StatusCode_t;
+
 typedef struct {
 	uint8_t 	sender_id;
 	uint8_t 	receiver_id;
-	uint16_t 	data_length;
+	uint8_t 	data_length;
 	uint8_t 	data[CP_MAX_DATA_LEN];
-	uint32_t	crc;
-} Frame_t;
+	uint16_t	crc;
+} CP_Frame_t;
 
-void 		CP_encode_data(uint8_t*, uint8_t*, uint8_t, uint8_t*);
-void 		CP_decode_data(uint8_t*, uint8_t*, uint8_t, uint8_t*);
-void 		CP_receive_frame();
-uint8_t 	CP_decode_received_frame(uint8_t*, uint8_t, Frame_t*);
-uint8_t		CP_2hex_to_byte(char, char);
-uint16_t	CP_hex_to_word(char, char, char, char);
-uint8_t 	CP_validate_frame(Frame_t* frame);
+void 			CP_encode_data(uint8_t*, uint8_t*, uint8_t, uint8_t*);
+void 			CP_decode_data(uint8_t*, uint8_t*, uint8_t, uint8_t*);
+void 			CP_receive_frame();
+CP_StatusCode_t CP_decode_received_frame(uint8_t*, uint8_t, CP_Frame_t*);
+CP_StatusCode_t	CP_2hex_to_byte(char, char, uint8_t*);
+CP_StatusCode_t	CP_hex_to_word(char, char, char, char, uint16_t*);
+uint8_t 		CP_validate_frame(CP_Frame_t* frame);
 
 /*
  * Funkcja do testowania działania UARTA
--- Core/Src/communication_protocol.c
+++ Core/Src/communication_protocol.c
@@ -26,7 +26,7 @@ void TEST_received_data() {
 }
 
 void CP_receive_frame() {
-	static uint8_t frame_buffer[412];
+	static uint8_t frame_buffer[CP_MAX_DATA_LEN*2 + CP_MIN_FRAME_LEN];
 	static uint8_t frame_index = 0;
 
 	uint8_t temp_byte;
@@ -44,47 +44,202 @@ void CP_receive_frame() {
 			frame_buffer[frame_index++] = temp_byte;
 		}
 
-		if(temp_byte == CP_END_CHAR && frame_index >= CP_MIN_DATA_LEN) {
-			Frame_t decoded_frame;
-			if(CP_decode_received_frame(frame_buffer, frame_index, &decoded_frame)) {
+		if(temp_byte == CP_END_CHAR) {
+			CP_Frame_t decoded_frame;
+			CP_StatusCode_t status;
+			char msg[100];
+			if((status = CP_decode_received_frame(frame_buffer, frame_index, &decoded_frame)) == DR_OK) {
 				// TODO: Dekodowanie ramki przebiegło pomyślnie, napisać przetwarzanie
+				snprintf(msg, sizeof(msg), "DECODE OK STATUS=%d\r\n", status);
+				UART_SendText(msg);
+				continue;
+			} else {
+				snprintf(msg, sizeof(msg), "DECODE ERROR STATUS=%d\r\n", status);
+				UART_SendText(msg);
+				continue;
 			}
 		}
 	}
 }
 
-uint8_t CP_decode_received_frame(uint8_t* frame_buffer, uint8_t frame_length, Frame_t* output) {
+CP_StatusCode_t CP_decode_received_frame(uint8_t* frame_buffer, uint8_t frame_length, CP_Frame_t* output) {
+	/*
+	 * Jeśli długość wczytanej ramki jest mniejsza
+	 * niż jej minimalna długość
+	 * zwróć błąd
+	 */
+	if(frame_length < CP_MIN_FRAME_LEN) return DR_FRAME_TOO_SHORT;
 
-	output->sender_id = CP_2hex_to_byte((char)frame_buffer[1], (char)frame_buffer[2]);
-	output->receiver_id = CP_2hex_to_byte((char)frame_buffer[3], (char)frame_buffer[4]);
-	output->data_length = CP_2hex_to_byte((char)frame_buffer[5], (char)frame_buffer[6]);
+	/*
+	 * Przełączenie w tryb czytania danych
+	 */
+	CP_DecodeState_t decode_state = DS_READ_DATA;
 
-	if(output->data_length > CP_MAX_DATA_LEN || output->data_length != frame_length - CP_MIN_DATA_LEN) {
-		UART_SendText("INVALID DATA LENGTH\r\n");
-		return 0;
+	/*
+	 * Ilość wczytanych i zdekodowanych znaków
+	 */
+	uint16_t data_counter = 0;
+
+	/*
+	 * Dane zaczynają się od 7 pozycji
+	 */
+	uint16_t data_index = CP_DATA_START_IDX;
+
+	/*
+	 * Wczytywanie podstawowych informacji z ramki
+	 * Jeśli znaleziono w nich nieprawidłowe znaki
+	 * należy zwrócić błąd.
+	 */
+	if(CP_2hex_to_byte((char)frame_buffer[1], (char)frame_buffer[2], &output->sender_id) 	== HEX_WRONG_CHAR) return HEX_WRONG_CHAR;
+	if(CP_2hex_to_byte((char)frame_buffer[3], (char)frame_buffer[4], &output->receiver_id) 	== HEX_WRONG_CHAR) return HEX_WRONG_CHAR;
+	if(CP_2hex_to_byte((char)frame_buffer[5], (char)frame_buffer[6], &output->data_length) 	== HEX_WRONG_CHAR) return HEX_WRONG_CHAR;
+
+
+	/*
+	 * Jeśli id odbiorcy nie zgadza się z id płytki
+	 * należy przestać dekodować ramkę.
+	 */
+	if(output->receiver_id != CP_STM_REC_ID) return DR_RECEIVER_DIFF;
+
+	/*
+	 * Jeśli odbiorca równa się nadawcy
+	 * należy zwrócić błąd
+	 */
+	if(output->receiver_id == output->sender_id) return DR_RECEIVER_EQUAL_SENDER;
+
+	/*
+	 * Jeśli zadeklarowano więcej niż 200 znaków w polu danych
+	 * należy zwrócić błąd
+	 */
+	if(output->data_length > 200) return DR_WRONG_DATA_LEN_PROVIDED_IN_FRAME;
+
+	// Teraz można przystąpić do wczytywania danych
+
+	/*
+	 * Program przechodzi przez dane i zwiększa data_counter o 1
+	 * z każdym zdekodowanym znakiem.
+	 *
+	 * Znak zakodowany np. sekwencja \1 jest traktowana jako jeden znak
+	 * pomimo że zajmuje dwa miejsca w zakodowanej ramce.
+	 *
+	 * Zadeklarowana długość danych dotyczy danych niekodowanych.
+	 */
+
+	/*
+	 * Maksymalna ilość danych jakie można wczytać to
+	 * całkowita długość przesłanej ramki odjąć jej minimalna długość.
+	 * Jest to rzeczywista długość danych przesłanych w ramce.
+	 */
+	uint16_t max_data_counter = frame_length - CP_MIN_FRAME_LEN;
+	/*
+	 * Jeśli powyższa wartość różni się od zadeklarowanej długości
+	 * danych należy zwrócić błąd
+	 */
+	if(max_data_counter != output->data_length) return DR_WRONG_DATA_LEN_PROVIDED_IN_FRAME;
+
+	while(data_counter < output->data_length) {
+		uint8_t temp_byte = frame_buffer[data_index];
+
+		if(decode_state == DS_READ_DATA) {
+			/*
+			 * Jeśli podczas czytania danych napotkano
+			 * znak kodowania
+			 * należy go pominąć i przełączyć się
+			 * w tryb dekodowania danych
+			 */
+			if(temp_byte == CP_ENCODE_CHAR) {
+				decode_state = DS_DECODE_NEXT_DATA;
+				data_index++;
+				continue;
+			}
+
+			output->data[data_counter] = temp_byte;
+			data_index++;
+			data_counter++;
+			continue;
+		} else if(decode_state == DS_DECODE_NEXT_DATA) {
+			/*
+			 * W trybie dekodowania danych oczekujemy trzech
+			 * znaków: 1, 2 lub 3.
+			 * Jeśli otrzymamy inny, jest to błędnie
+			 * zakodowana ramka.
+			 */
+			uint8_t decoded_byte;
+			switch(temp_byte) {
+				case CP_START_CODE_CHAR:
+					decoded_byte = CP_START_CHAR;
+					break;
+				case CP_END_CODE_CHAR:
+					decoded_byte = CP_END_CHAR;
+					break;
+				case CP_ENCODE_CODE_CHAR:
+					decoded_byte = CP_ENCODE_CODE_CHAR;
+					break;
+				default:
+					// Zwrócenie błędu o nieprawidłowym znaku kodowania
+					return DR_WRONG_NEXT_CHARACTER;
+
+			}
+
+			// Zapisanie zdekodowanego znaku do ramki wyjściowej
+			output->data[data_counter] = decoded_byte;
+			data_counter++;
+			data_index++;
+			decode_state = DS_READ_DATA;
+			continue;
+		}
 	}
 
-	for(uint8_t data_index = 0, decode_index = 7; data_index < output->data_length; data_index++) {
-		output->data[data_index] = frame_buffer[decode_index++];
+	// Po zakończeniu przetwarzania danych sprawdź poprawność
+
+	/*
+	 * Jeśli zakończono wczytywać dane oczekując znaku do zdekodowania
+	 * należy zwrócić błąd
+	 */
+	if(decode_state == DS_DECODE_NEXT_DATA) return DR_DATA_END_WHILE_DECODE;
+
+	// Jeśli dane zostały wczytane bez błędu, można zdekodować CRC
+
+	/*
+	 * Jeśli CRC było niepoprawnie zakodowane
+	 * należy zwrócić błąd.
+	 */
+	if(CP_hex_to_word(	frame_buffer[data_index],
+						frame_buffer[data_index+1],
+						frame_buffer[data_index+2],
+						frame_buffer[data_index+3],
+						&output->crc) == HEX_WRONG_CHAR) {
+		return HEX_WRONG_CHAR;
 	}
 
-	uint8_t crc_start_index = 6 + output->data_length;
-
-	output->crc = CP_hex_to_word(frame_buffer[crc_start_index+1], frame_buffer[crc_start_index+2], frame_buffer[crc_start_index+3], frame_buffer[crc_start_index+4]);
-
-	UART_SendText("DECODED FRAME\r\n");
-	return 1;
+	// Poprawnie zdekodowano całą ramkę
+	return DR_OK;
 }
 
-uint8_t	CP_2hex_to_byte(char high, char low) {
+CP_StatusCode_t	CP_2hex_to_byte(char high, char low, uint8_t* output) {
 	high = toupper(high);
 	low = toupper(low);
 
-	return ((high >= 'A' ? high - 'A' + 10 : high - '0') << 4) |
+	if(!((high >= '0' && high <= '9') || (high >= 'A' && high <= 'F')) ||
+			!((low >= '0' && low <= '9') || (low >= 'A' && low <= 'F'))) {
+		return HEX_WRONG_CHAR;
+	}
+
+	*output = ((high >= 'A' ? high - 'A' + 10 : high - '0') << 4) |
 			(low >= 'A' ? low - 'A' + 10 : low - '0');
+
+	return HEX_OK;
 }
 
-uint16_t CP_hex_to_word(char high1, char low1, char high2, char low2) {
-	return (CP_2hex_to_byte(high1, low1) << 8) | CP_2hex_to_byte(high2, low2);
+CP_StatusCode_t CP_hex_to_word(char high1, char low1, char high2, char low2, uint16_t* output) {
+	uint8_t byte1;
+	uint8_t byte2;
+
+	if(CP_2hex_to_byte(high1, low1, &byte1) == HEX_WRONG_CHAR) return HEX_WRONG_CHAR;
+	if(CP_2hex_to_byte(high2, low2, &byte2) == HEX_WRONG_CHAR) return HEX_WRONG_CHAR;
+
+	*output = (byte1 << 8) | byte2;
+
+	return HEX_OK;
 }
 
